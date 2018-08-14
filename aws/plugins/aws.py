aa38f6aa363eee49e465ba0a0005bcfc575ebe11
Cloud User <centos@ip-10-206-32-82.affinionds.com>
2018-08-14 15:55:36 +0000

    adding aws modules

---
+++ aws/plugins/aws.py
@@ -0,0 +1,55 @@
+import os,time,glob
+import subprocess
+from subprocess import Popen, PIPE
+from slackbot.bot import respond_to
+from slackbot.bot import listen_to
+import re
+
+@respond_to('hi', re.IGNORECASE)
+def hi(message):
+    message.reply('I can understand hi or HI!')
+    # react with thumb up emoji
+    message.react('+1')
+
+@respond_to('I love you')
+def love(message):
+    message.reply('I love you too!')
+
+
+@respond_to('kplay')
+def love(message):
+    message.reply('playing Osmc Now!')
+    os.system('kodi-send --action="PlayMedia({})"').format(message)
+'''
+@respond_to('kboot',re.IGNORECASE)
+def love(message):
+    message.reply('Reboting Osmc Now!')
+    Popen(["sudo","systemctl","restart","mediacenter" ], stdout=PIPE, stderr=PIPE)
+
+aws ec2 describe-instances --query 'Reservations[].Instances[].[Tags[?Key==`Name`].Value[],InstanceType,PrivateIpAddress,LaunchTime]' --output text | sed '$!N;s/\n/ /'
+
+'''
+
+@respond_to('aws-dev',re.IGNORECASE)
+def love(message):
+    message.reply('Getting list of dev!')
+#    Popen(["aws ec2 describe-instances --query 'Reservations[].Instances[].[Tags[?Key==`Name`].Value[],InstanceType,PrivateIpAddress,LaunchTime]' --output text | sed '$!N;s/\n/ /'" ], stdout=PIPE, stderr=PIPE)
+#    rk = os.system("aws ec2 describe-instances --query 'Reservations[].Instances[].[Tags[?Key==`Name`].Value[],InstanceType,PrivateIpAddress,LaunchTime]' --output text " )
+#    pk=subprocess.check_output("aws ec2 describe-instances --query 'Reservations[].Instances[].[Tags[?Key==`Name`].Value[],InstanceType,PrivateIpAddress,LaunchTime]' --output text", shell=True).strip()
+#    tk=subprocess.check_output(["aws ec2 describe-instances --query 'Reservations[].Instances[].[Tags[?Key==`Name`].Value[],InstanceType,PrivateIpAddress,LaunchTime]' --output text", 'sed', '$!N;s/\n/ /'], shell=True)
+#    tk=subprocess.Popen("aws ec2 describe-instances --query 'Reservations[].Instances[].[Tags[?Key==`Name`].Value[],InstanceType,PrivateIpAddress]' --output text | sed 's/.*: //g' | sed 's/,/ \//g'", shell=True, stdout=subprocess.PIPE, stderr=subprocess.STDOUT).communicate()[0].strip()
+#    ck=subprocess.check_output( 'aws ec2 describe-instances --output text --query "PrivateIpAddress,Reservations[*].Instances[*].[InstanceId,State.Name,Tags[0].Value]"', shell=True, stderr=subprocess.STDOUT )
+    tk=subprocess.check_output( 'aws ec2 describe-instances --output text --query "Reservations[*].Instances[*].[PrivateIpAddress,InstanceId,State.Name,Tags[0].Value]"', shell=True, stderr=subprocess.STDOUT )
+
+    message.reply(tk)
+
+@listen_to('Can someone help me?')
+def help(message):
+    # Message is replied to the sender (prefixed with @user)
+    message.reply('Yes, I can!')
+
+    # Message is sent on the channel
+    message.send('I can help everybody!')
+
+    # Start a thread on the original message
+    message.reply("Here's a threaded reply", in_thread=True)
